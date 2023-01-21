ed23b8a4c156fd8a5bc213d07909216c1909ad1e
vladlen <konqrosy3233vcay6sofhzxwnioq5g32xowomguz4axq42v3db3q>
2023-01-21 15:18:45 +0500

    update env file

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,9 @@
 import {removeCookie, setCookie} from "./cookie"
 
 
-if(!process.env.REACT_APP_DOMAIN_API) throw Error("Error REACT_APP_DOMAIN_API not find!")
+if(!process.env.APP_DOMAIN_API) throw Error("Error REACT_APP_DOMAIN_API not find!")
 
-export const DOMAIN = process.env.REACT_APP_DOMAIN_API
+export const DOMAIN = process.env.APP_DOMAIN_API
 export const DOMAIN_API = `${DOMAIN}/api`
 
 const TOKEN_NAME = "site_token_access"
